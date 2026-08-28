95edd568d93a0ff5b3cbe6fd38328dbcf11f3a75
Xavier <xavier.villemin@gmail.com>
2026-08-28 18:30:15 +0200

    Entités : mise en place des groupes de dépendances

---
+++ XaliseConnect.Domain/Entities/Workflow/WorkflowDependencyGroup.cs
@@ -0,0 +1,51 @@
+﻿using XaliseConnect.Domain.Entities.Reference;
+
+namespace XaliseConnect.Domain.Entities.Workflow
+{
+    /// <summary>
+    /// Groupe de dépendances d'un flux.
+    /// </summary>
+    public sealed class WorkflowDependencyGroup : BaseEntity
+    {
+        /// <summary>
+        /// Identifiant de l'événement du flux associé au groupe de dépendances.
+        /// </summary>
+        public int WorkflowEventId { get; private set; }
+
+        /// <summary>
+        /// Événement du flux associé au groupe de dépendances.
+        /// </summary>
+        public WorkflowEvent WorkflowEvent { get; private set; } = null!;
+
+        /// <summary>
+        /// Identifiant du type de dépendance du flux associé au groupe de dépendances.
+        /// </summary>
+        public int WorkflowDependencyTypeId { get; private set; }
+
+        /// <summary>
+        /// Type de dépendance du flux associé au groupe de dépendances.
+        /// </summary>
+        public WorkflowDependencyType WorkflowDependencyType { get; private set; } = null!;
+
+        /// <summary>
+        /// Constructeur réservé à l'infrastructure (EF Core).
+        /// </summary>
+        private WorkflowDependencyGroup() { }
+
+        /// <summary>
+        /// Constructeur public pour créer une instance de <see cref="WorkflowDependencyGroup"/> avec un événement du flux et un type de dépendance du flux.
+        /// </summary>
+        /// <param name="workflowEvent">Événement du flux associé au groupe de dépendances.</param>
+        /// <param name="workflowDependencyType">Type de dépendance du flux associé au groupe de dépendances.</param>
+        public WorkflowDependencyGroup(WorkflowEvent workflowEvent, WorkflowDependencyType workflowDependencyType)
+        {
+            ArgumentNullException.ThrowIfNull(workflowEvent, nameof(workflowEvent));
+            ArgumentNullException.ThrowIfNull(workflowDependencyType, nameof(workflowDependencyType));
+
+            this.WorkflowEvent = workflowEvent;
+            this.WorkflowEventId = workflowEvent.Id;
+            this.WorkflowDependencyType = workflowDependencyType;
+            this.WorkflowDependencyTypeId = workflowDependencyType.Id;
+        }
+    }
+}
---
+++ XaliseConnect.Domain/Entities/Workflow/WorkflowDependencyItem.cs
@@ -0,0 +1,49 @@
+﻿namespace XaliseConnect.Domain.Entities.Workflow
+{
+    /// <summary>
+    /// Représente un élément de dépendance dans un flux de travail.
+    /// </summary>
+    public sealed class WorkflowDependencyItem : BaseEntity
+    {
+        /// <summary>
+        /// Identifiant du groupe de dépendances auquel appartient l'élément de dépendance.
+        /// </summary>
+        public int WorkflowDependencyGroupId { get; private set; }
+
+        /// <summary>
+        /// Groupe de dépendances auquel appartient l'élément de dépendance.
+        /// </summary>
+        public WorkflowDependencyGroup WorkflowDependencyGroup { get; private set; } = null!;
+
+        /// <summary>
+        /// Événement du flux auquel appartient l'élément de dépendance.
+        /// </summary>
+        public int WorkflowEventId { get; private set; }
+
+        /// <summary>
+        /// Événement du flux auquel appartient l'élément de dépendance.
+        /// </summary>
+        public WorkflowEvent WorkflowEvent { get; private set; } = null!;
+
+        /// <summary>
+        /// Constructeur réservé à l'infrastructure (EF Core).
+        /// </summary>
+        private WorkflowDependencyItem() { }
+
+        /// <summary>
+        /// Constructeur public pour créer une instance de <see cref="WorkflowDependencyItem"/> avec un groupe de dépendances et un événement du flux.
+        /// </summary>
+        /// <param name="workflowDependencyGroup">Groupe de dépendances auquel appartient l'élément de dépendance.</param>
+        /// <param name="workflowEvent">Événement du flux auquel appartient l'élément de dépendance.</param>
+        public WorkflowDependencyItem(WorkflowDependencyGroup workflowDependencyGroup, WorkflowEvent workflowEvent)
+        {
+            ArgumentNullException.ThrowIfNull(workflowDependencyGroup, nameof(workflowDependencyGroup));
+            ArgumentNullException.ThrowIfNull(workflowEvent, nameof(workflowEvent));
+
+            this.WorkflowDependencyGroup = workflowDependencyGroup;
+            this.WorkflowDependencyGroupId = workflowDependencyGroup.Id;
+            this.WorkflowEvent = workflowEvent;
+            this.WorkflowEventId = workflowEvent.Id;
+        }
+    }
+}
