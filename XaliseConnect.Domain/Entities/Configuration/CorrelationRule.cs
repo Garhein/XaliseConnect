4f09acd3337f6a98a4b81c044fd073d394946d85
Xavier <xavier.villemin@gmail.com>
2026-08-28 15:35:51 +0200

    Entités : mise en place de la règle de corrélation

---
+++ XaliseConnect.Domain/Entities/Configuration/CorrelationRule.cs
@@ -0,0 +1,45 @@
+﻿namespace XaliseConnect.Domain.Entities.Configuration
+{
+    /// <summary>
+    /// Règle de corrélation permettant de traiter un flux.
+    /// </summary>
+    public sealed class CorrelationRule : BaseEntity
+    {
+        /// <summary>
+        /// Libellé de la règle de corrélation.
+        /// </summary>
+        public string Label { get; private set; } = string.Empty;
+
+        /// <summary>
+        /// Description de la règle de corrélation.
+        /// </summary>
+        public string Description { get; private set; } = string.Empty;
+
+        /// <summary>
+        /// Expression de la règle de corrélation.
+        /// </summary>
+        public string Expression { get; private set; } = string.Empty;
+
+        /// <summary>
+        /// Constructeur réservé à l'infrastructure (EF Core).
+        /// </summary>
+        private CorrelationRule() { }
+
+        /// <summary>
+        /// Constructeur public pour créer une instance de <see cref="CorrelationRule"/> avec un libellé, une description et une expression.
+        /// </summary>
+        /// <param name="label">Libellé de la règle de corrélation.</param>
+        /// <param name="description">Description de la règle de corrélation.</param>
+        /// <param name="expression">Expression de la règle de corrélation.</param>
+        public CorrelationRule(string label, string description, string expression)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(label, nameof(label));
+            ArgumentNullException.ThrowIfNull(description, nameof(description));
+            ArgumentException.ThrowIfNullOrWhiteSpace(expression, nameof(expression));
+
+            this.Label = label;
+            this.Description = description;
+            this.Expression = expression;
+        }
+    }
+}
